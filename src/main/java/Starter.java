4b2401e9b7171ba486fcbe280932e439c3b7de91
Shashith-Malshan <malshanshashith318@gmail.com>
2025-09-28 13:00:18 +0530

    Add Main Login Form(.fxml)

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -3,9 +3,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
 public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/MainLoginForm.fxml"))));
+        stage.show();
     }
 }
