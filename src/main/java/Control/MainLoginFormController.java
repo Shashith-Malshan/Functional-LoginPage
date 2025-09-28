3f2e248dd7df25d140c69c28eba01d108c1efbf9
Shashith-Malshan <malshanshashith318@gmail.com>
2025-09-28 16:05:32 +0530

    Add New Account Form

--- src/main/java/Control/MainLoginFormController.java
+++ src/main/java/Control/MainLoginFormController.java
@@ -1,11 +1,16 @@
 package Control;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainLoginFormController {
     public TextField txtEmail;
@@ -18,11 +23,30 @@ public class MainLoginFormController {
         System.out.println("success");
     }
 
+    Stage resetStage=new Stage();
+
     public void clickedReset(MouseEvent mouseEvent) {
-        System.out.println("Reset");
+        Stage stage = (Stage) lblReset.getScene().getWindow();
+        stage.close();
+
+        try {
+            resetStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/ResetPasswordForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        resetStage.show();
     }
 
+    Stage newStage=new Stage();
     public void clickedNew(MouseEvent mouseEvent) {
-        System.out.println("New");
+        Stage stage = (Stage) lblNew.getScene().getWindow();
+        stage.close();
+        try {
+            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/NewAccountForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        newStage.show();
     }
 }
---
+++ src/main/java/Control/NewAccountFormController.java
@@ -0,0 +1,29 @@
+package Control;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+public class NewAccountFormController {
+    public TextField txtEmail;
+    public Label lblEmail;
+    public PasswordField txtPassword;
+    public Button btnCreate;
+    public Label lblLog;
+    public Button btnGetOtp;
+    public TextField txtOtp;
+    public Label lblResend;
+    public CheckBox checkLogged;
+
+    public void actionCreate(ActionEvent actionEvent) {
+    }
+
+    public void clickedLog(MouseEvent mouseEvent) {
+    }
+
+    public void actionGetOtp(ActionEvent actionEvent) {
+    }
+
+    public void clickedResend(MouseEvent mouseEvent) {
+    }
+}
