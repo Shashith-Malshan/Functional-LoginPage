e5d512bc266477898c193fa12dd2dc7635447196
Shashith-Malshan <malshanshashith318@gmail.com>
2025-09-28 13:52:57 +0530

    Add Reset Password Form

--- src/main/java/Control/ResetPasswordFormController.java
+++ src/main/java/Control/ResetPasswordFormController.java
@@ -1,4 +1,32 @@
 package Control;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class ResetPasswordFormController {
+    public TextField txtEmail;
+    public PasswordField txtNewPassword;
+    public Button btnSubmit;
+    public Label lblNew;
+    public PasswordField txtConfirmPassword;
+
+    public void validateEmail(KeyEvent keyEvent) {
+    }
+
+    public void validateNewPassword(KeyEvent keyEvent) {
+    }
+
+    public void actionSubmit(ActionEvent actionEvent) {
+    }
+
+    public void clickedBack(MouseEvent mouseEvent) {
+    }
+
+    public void validateConfirmPassword(KeyEvent keyEvent) {
+    }
 }
